edcb4857015357062c8a2ee777c97c69b65a64f8
sydneyani <135778566+sydneyani@users.noreply.github.com>
2023-06-17 15:27:29 -0500

    Delete ZyLab1-20.py

--- ZyLab1-20.py
+++
@@ -1,9 +0,0 @@
-#Sydney Ani PID:1869076
-
-user_num = int(input("Enter integer:\n"))
-print("You entered:", user_num)
-print(user_num, "squared is", user_num ** 2)
-print("And", user_num, "cubed is", user_num ** 3, "!!")
-user_num2 = int(input("Enter another integer:\n"))
-print(user_num, "+", user_num2, "is", user_num2 + user_num)
-print(user_num, "*", user_num2, "is", user_num2 * user_num)

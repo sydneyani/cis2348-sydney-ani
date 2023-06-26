7df6419fa93bdbacddb564f737aec2f075b19fd5
sydneyani <135778566+sydneyani@users.noreply.github.com>
2023-06-26 13:30:16 -0500

    Add files via upload

---
+++ Homework2/CodingProblem2.py
@@ -0,0 +1,24 @@
+Problem 1: Password Strengthener
+
+Description:
+The program takes a simple password and makes it stronger by replacing characters using a specified key and appending a suffix.
+
+Problem 2: Linear Equation Solver
+
+Description:
+The program solves a set of linear equations with integer coefficients to find an integer solution for x and y within a given range.
+
+Problem 3: Coin Change
+
+Description:
+The program takes a total change amount as an integer input and outputs the minimum number of coins needed to make that change, using the fewest coins possible. The available coin types are dollars, quarters, dimes, nickels, and pennies. The program is implemented using a function exact_change()
+
+Problem 4: Palindrome Checker
+
+Description:
+The program checks whether a given word or phrase is a palindrome. A palindrome is a word or phrase that reads the same forwards and backwards, disregarding spaces. The program implements a function is_palindrome() to determine if a word is a palindrome.
+
+Problem 5: Word Frequency Counter
+
+Description:
+The program reads a CSV file containing words separated by commas and outputs the unique words along with their frequencies. It eliminates duplicates and provides a count of how many times each word appears in the file. The program utilizes the csv module in Python for reading the CSV file.
